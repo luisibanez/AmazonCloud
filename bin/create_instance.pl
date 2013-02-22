6b245dd9ccde4092f5447496bf6592ef02597529
Quang Trinh <quang.trinh@gmail.com>
2013-02-21 19:50:17 -0500

    updated  bin/create_instance.pl

--- bin/create_instance.pl
+++ bin/create_instance.pl
@@ -4,8 +4,8 @@
 # written by the following people from modENCODE DCC group:
 # Ziru Zhou, ziruzhou@gmail.com
 # Kar Ming Chu, mr.kar.ming.chu@gmail.com
-# Quang Trinh, quang.trinh@gmail.com
 # Fei-Yang(Arthur) Jen
+# Quang Trinh, quang.trinh@gmail.com
 #
 
 use strict;
