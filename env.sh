245cbed83d39e961175073ec402e52714b4af202
Fjen <arthur20249@gmail.com>
2013-03-04 16:21:27 -0500

    updated env.sh

--- env.sh
+++ env.sh
@@ -1,16 +1,17 @@
 
 # set JAVA_HOME
+# For Mac users, you can set your JAVA_HOME variable to: `/System/Library/Frameworks/JavaVM.framework/Versions/Current/Commands/java_home`
 export JAVA_HOME=your_JAVA_HOME_PATH
 
 # set your AWS credentials 
 export AWS_ACCESS_KEY=your_AWS_ACCESS_KEY_ID 
 export AWS_SECRET_KEY=your_AWS_SECRET_KEY
 
-############################################
-# no changes are needed below this line
-############################################
+###########################################
+# no changes are needed below this line   #
+###########################################
 
 # set EC2_HOME and add $EC2_HOME/bin to $PATH
-export EC2_HOME=./external_tools/ec2-api-tools-1.6.1.4
+export EC2_HOME=`pwd`/external_tools/ec2-api-tools-1.6.1.4
 export PATH=$PATH:$EC2_HOME/bin
 
